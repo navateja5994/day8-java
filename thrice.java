766d14547e2cc16c70af2fcc6af2dafc789d2be8
NAVATEJA <navatejar80@gmail.com>
2024-12-09 10:55:28 +0530

    Add files via upload

---
+++ thrice.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int ns[]=new int[n];
+        int k=0,s=0;
+        for(int i=1;i<=n;i++)
+        {
+            s+=i;
+            ns[k]=s;
+            k++;
+        }
+        for(int i=0;i<n;i+=3)
+        {
+            System.out.println(ns[i]+" ");
+        }
+    }
+}
