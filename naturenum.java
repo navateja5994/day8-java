c326a42078993b6f605b45e781e6d160d366b38c
NAVATEJA <navatejar80@gmail.com>
2024-12-09 10:19:34 +0530

    Add files via upload

---
+++ naturenum.java
@@ -0,0 +1,17 @@
+ class Main {
+    public static void main(String[] args) {
+        int n=5; 
+        int ns[] = new int[n];
+        int sum = 0;
+        
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+            ns[i - 1] = sum;
+        }
+        
+        
+        for (int num : ns) {
+            System.out.print(num + " ");
+        }
+    }
+}
