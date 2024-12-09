2a6f5cb3f917ad73f873610c4d2a902a10405bcc
NAVATEJA <navatejar80@gmail.com>
2024-12-09 13:40:42 +0530

    Add files via upload

---
+++ 2darr.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class arr
+{
+    public static void main(String args[])
+    {
+        //initializing of 2D array
+        int a[][]={{1,2},{3,4}};
+        //accessing 1D array in 2 array
+        System.out.println(Arrays.toString(a[0]));
+        System.out.println(Arrays.toString(a[1]));
+
+    }
+}
